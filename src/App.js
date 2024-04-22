b4e391498aa3dee28a60f0a4fbc9b9e9098e5bff
codewithtarde9370 <teja36491@gmail.com>
2024-04-23 00:47:09 +0530

    react todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import { TiDeleteOutline } from "react-icons/ti";
+
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="todo-title">My Tasks Lists</h1>
+      <div className="todo-container">
+        <div className="todo-input-container">
+          <div className="todo-inputs">
+            <label>Title</label>
+            <input type="text" placeholder="Title of task"></input>
+          </div>
+          <div className="todo-inputs">
+            <label>Description</label>
+            <input type="text" placeholder="Describe your task"></input>
+          </div>
+          <div className="todo-inputs">
+            
+          <button type="button" className="primary-btn">Add</button>
+          </div>
+
+        </div>
+        <div className="todo-btn-container">
+
+          <button type="button" className={`task-btn ${isCompleteScreen === false ?'active' : ''}`} onClick={() => setIsCompleteScreen (false)} >Tasks</button>
+
+          <button type="button" className={`task-btn ${isCompleteScreen === true ?'active': ''}`} onClick={() => setIsCompleteScreen (true)}>Completed</button>
+        </div>
+
+          <div className="todo-task-list">
+              <div className="todo-list-items">
+                <h3>Task 1</h3>
+                <p>Description</p>
+
+              </div>
+
+          </div>
+      </div>
     </div>
   );
 }
